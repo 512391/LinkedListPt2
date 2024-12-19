e5845e48628c4afa324fc5d9d0a391b3bc178471
512391 <512391@bsd48.org>
2024-12-19 12:23:13 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,16 +1,20 @@
 #include<iostream>
 /*
 Author: jay williamson
+date:12/19/24
+this is the .h that holds the student info
   */
   class Student
   {
     public:
+//all the students information
     char firstName[20];
     char lastName[20];
     float GPA;
     int id;
     int lastNameLength;
     int firstNameLength;
+//random seed for hashing distribution
     int randomSeed;
     Student();
   };
